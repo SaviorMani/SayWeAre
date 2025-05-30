626bdeda400bf4f154c1e2bd8879d043dcab8b92
SaviorMani <213963685+SaviorMani@users.noreply.github.com>
2025-05-30 16:12:47 +0530

    Your message

--- app/src/main/java/com/manideep/sayweare/MainActivity.kt
+++ app/src/main/java/com/manideep/sayweare/MainActivity.kt
@@ -4,7 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxSizegit push -u origin main
+
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
